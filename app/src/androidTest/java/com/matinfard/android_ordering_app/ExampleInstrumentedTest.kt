0d83fba4756172186928c076dab5e965080c028d
Yousef Matinfard <matinfard.y@gmail.com>
2020-04-12 23:49:03 +0200

    fix package name error in instrumented test

--- app/src/androidTest/java/com/matinfard/android_ordering_app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/matinfard/android_ordering_app/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.matinfard.android_ordering_app", appContext.packageName)
+        assertEquals("com.matinfard.androidorderingapp", appContext.packageName)
     }
 }
